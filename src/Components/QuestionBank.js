2f52d4993547c5a2ce71ae0fda353dca82578e69
Krunal Trivedi <krunalstrivedi@gmail.com>
2023-07-31 11:54:29 +0530

    Add files via upload

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,36 @@
+const qBank = [
+	{
+		id: 1,
+		question: "What is the HTML Full form?",
+		options: ["HyperText Markup Language", "Hyper Markup Language",
+        "HyperText Language", "HyperText Mark Language"],
+		answer: "HyperText Markup Language",
+	},
+	{
+		id: 2,
+		question: "What is the Css full form",
+		options: ["Cascading System Sheets", "Cascading Style Sheets ",
+         "Cascading Style Setting", "Casca Style Sheets"],
+		answer: "Cascading Style Sheets",
+	},
+	{
+		id: 3,
+		question: "React wad created by?",
+		options: ["john", "john Doe", "jordan Doe", "Jordan Walke,"],
+		answer: "Jordan Walke,"
+	},
+	{
+		id: 4,
+		question: "Which is the Initial release date of React?",
+		options: ["29 May 2013", "29 May 2017", "29 june 2013", "31 july 2013"],
+		answer: "29 May 2013"
+	},
+	{
+		id: 5,
+		question: "What language is used in ReactJS?",
+		options: ["CSS", "JAVA", "JSX", "PHP"],
+		answer: "JSX"
+	},
+]
+
+export default qBank;
